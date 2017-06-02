33d8cd49de0b9dd74b9594d34b7c78916b65b9a9
NachMS <nakamasasctm@yahoo.co.jp>
2017-06-02 14:48:11 +0900

    Studentクラスの編集とCourse、Orderクラス作成

---
+++ src/models/Course.java
@@ -0,0 +1,90 @@
+package models;
+
+public class Course {
+	private int courseID;
+	private String name;
+	private String teacher;
+	private int grade;
+	private int semester;
+	private int day;
+	private int hour;
+	private int likes;
+	private int dislikes;
+
+	public Course() {
+
+	}
+
+	public int getCourseID() {
+		return courseID;
+	}
+
+	public void setCourseID(int courseID) {
+		this.courseID = courseID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getSemester() {
+		return semester;
+	}
+
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	public int getDislikes() {
+		return dislikes;
+	}
+
+	public void setDislikes(int dislikes) {
+		this.dislikes = dislikes;
+	}
+
+}
--- src/models/Student.java
+++ src/models/Student.java
@@ -3,12 +3,26 @@ package models;
 import java.io.Serializable;
 
 public class Student implements Serializable {
+	private String studentID;
 	private String name;
+	private String faculty;
+	private String department;
+	private int grade;
 	private String pass;
+	//private ArrayList<Course> courses;
 
 	public Student() {
 		name = "";
 		pass = "";
+	}
+
+
+	public String getStudentID() {
+	    return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+	    this.studentID = studentID;
 	}
 
 	public String getName() {
@@ -17,6 +31,39 @@ public class Student implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+
+	public String getFaculty() {
+	    return faculty;
+	}
+
+
+
+	public void setFaculty(String faculty) {
+	    this.faculty = faculty;
+	}
+
+
+
+	public String getDepartment() {
+	    return department;
+	}
+
+
+
+	public void setDepartment(String department) {
+	    this.department = department;
+	}
+
+
+	public int getGrade() {
+	    return grade;
+	}
+
+
+	public void setGrade(int grade) {
+	    this.grade = grade;
 	}
 
 	public String getPass() {

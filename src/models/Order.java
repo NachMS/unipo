45e21deb354dc56fb273cb2b15c8e398f31e6fb1
fairytail7 <teriosu7778@gmail.com>
2017-06-02 21:23:27 +0900

    とりあえずCourseとTextbookのクラスは作ったOrderのクラスはbooleanDatetimeの所はまだ

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,5 +1,29 @@
 package models;
 
 public class Order {
-private int;
+private int orderID;
+private String studentID;
+private int totalAmount;
+//private ArrayList<Textbook> Textbook;
+
+public int getOrderID() {
+	return orderID;
+}
+public void setOrderID(int orderID) {
+	this.orderID = orderID;
+}
+public String getStudentID() {
+	return studentID;
+}
+public void setStudentID(String studentID) {
+	this.studentID = studentID;
+}
+public int getTotalAmount() {
+	return totalAmount;
+}
+public void setTotalAmount(int totalAmount) {
+	this.totalAmount = totalAmount;
+}
+
+
 }

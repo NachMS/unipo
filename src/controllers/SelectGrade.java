c9f2ad3be67200f8c95305917160d3f6b4f0bb8d
hmmrjn <rodhamjun@hotmail.com>
2017-07-14 14:18:37 +0900

    選択した学部、学科、学年を学生テーブルに保存する機能

--- src/controllers/SelectGrade.java
+++ src/controllers/SelectGrade.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import models.Student;
+import models.StudentDAO;
 
 @WebServlet("/SelectGrade")
 public class SelectGrade extends HttpServlet {
@@ -40,13 +41,18 @@ public class SelectGrade extends HttpServlet {
 			log("session.student:" + student);
 			if (student.getCourses() != null && student.getGrade() != 0) {
 				if (student.getGrade() != gradeInt) {
-					log("G006から戻ってきて別の学年を選んだ場合セッションから科目を破棄");
+					log("履修科目選択画面から戻ってきて別の学年を選んだ場合、学生セッションから履修科目を破棄");
 					student.setCourses(null);
 				}
 			}
 			log("学生が選択した学年" + request.getParameter("grade") + "をsession.student.gradeに格納します。");
 			student.setGrade(gradeInt);
 			log("session.student:" + student);
+			/*
+			 * (非DT) 学生が選択した学部、学科、学年を学生テーブルに保存します
+			 */
+			StudentDAO sdao = new StudentDAO();
+			sdao.updateStudentAffiliation(student);
 			log("CourseTableにリダイレクトします");
 			response.sendRedirect("CourseTable");
 			return;

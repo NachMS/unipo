85b6decbdcd8edcb0ca34340c8d1287a4b9255cd
hmmrjn <rodhamjun@hotmail.com>
2017-07-16 14:58:38 +0900

    中沢のキャンセル機能がガバガバなので修正

--- src/controllers/CancelOrder.java
+++ src/controllers/CancelOrder.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import models.Order;
 import models.OrderDAO;
+import models.Student;
 
 @WebServlet("/CancelOrder")
 public class CancelOrder extends HttpServlet {
@@ -22,17 +24,52 @@ public class CancelOrder extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		// 未ログインの場合ログイン画面ヘ転送
+		/**
+		 * (例外) 未ログインの場合ログイン画面ヘ転送
+		 */
 		if (session.getAttribute("login") == null || !(Boolean) session.getAttribute("login")) {
 			response.sendRedirect("Login");
 			return;
 		}
-		int orderSelection = Integer.parseInt(request.getParameter("selection"));
-		OrderDAO dao = new OrderDAO();
-		boolean cancelFlag = dao.cancelOrderByID(orderSelection);
-		if (cancelFlag) {
-			response.sendRedirect("OrderHistory");
+		if (session.getAttribute("student") == null) {
+			response.sendRedirect("Logout");
+			return;
 		}
+
+		/**
+		 * (例外) URLにパラメータ ?id=(注文ID) がないとホームへ
+		 */
+		if (request.getParameter("id") == null) {
+			log("URLにパラメータ ?id=(注文ID) がないのでホームへ");
+			response.sendRedirect("Home");
+			return;
+		}
+
+		Student student = (Student) session.getAttribute("student");
+		int orderID = Integer.parseInt(request.getParameter("id"));
+		OrderDAO odao = new OrderDAO();
+		Order oldOrder = odao.getOrderByID(orderID);
+
+		/*
+		 * (例外) 注文はログイン中の学生の注文か確認
+		 */
+		if (!oldOrder.getStudentID().equals(student.getStudentID())) {
+			log("学生の注文ではありません。");
+			response.sendRedirect("Home");
+			return;
+		}
+
+		/*
+		 * 注文のキャンセル
+		 */
+		if (odao.cancelOrderByID(orderID)) {
+			String[] message = { "success", "注文をキャンセルしました。" };
+			session.setAttribute("message", message);
+		} else {
+			String[] message = { "error", "エラーが発生したため、注文をキャンセルできませんでした。" };
+			session.setAttribute("message", message);
+		}
+		response.sendRedirect("OrderHistory");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
